b2d7b50c48b886dabdf5335968452a1db25d60c6
Анастасия <natya.sokol2013@yandex.ru>
2022-07-04 14:11:36 +0500

    1v

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -2,14 +2,21 @@ window.onload = function() {
 const menuLink = document.querySelectorAll(".menu__link");
 const menuItem = document.querySelectorAll(".menu__item");
 const menu = document.querySelectorAll("ul .menu .menu_sub");
-
-for(let i = 0; i > menuLink.length; i++) {
-	menuLink.item(i).onclick = menuActive;
-}
+menu.classList.add("menu_active");
+Array.from(menuLink);
+Array.from(menuItem);
 function menuActive() {
-	menuLink.closest(".menu__item");
-if(menuLink.closest(".menu__item") === true) {
-	menu.item(i).classList.toggle("menu_active");
-}
-}
+	 for(let i = 0; i > menuLink.length; i++) {
+		if(menuLink.item(i).closest("menu__item")) {
+			menu.item(i).classList.toggle("menu_active");
+			menuLink.item(i).onclick = () => {
+				return false;
+			}
+		}
+		}
+	}
+	for(let i = 0; i > menuLink.length; i++){
+		menuLink.item(i).onclick = menuActive;
+	}
+
 }
--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -4,12 +4,12 @@ const close = document.getElementsByClassName("modal__close");
 const succes = document.getElementsByClassName("show-success");
 const modalSucces = document.getElementById("modal_success");
 modalMain.classList.add("modal_active");
-close.item(0).onclick = function() {
+close.onclick = function() {
     modalMain.classList.remove("modal_active");
 }
 succes.item(0).onclick = modal;
 function modal() {
-modalSucces.classList.add("modal_active");
+modalSucces.classList.toggle("modal_active");
 }
 }
 
