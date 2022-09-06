c1b552b6df1eeb19e25a429f89d4727c2d0ef075
Анастасия <natya.sokol2013@yandex.ru>
2022-09-06 16:09:04 +0500

    added

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -2,11 +2,11 @@ const tabs = Array.from(document.querySelectorAll(".tab"));;
 const tabContents = Array.from(document.querySelectorAll(".tab__content"));
 for(let i=0; i<tabs.length;i++) {
     tabs[i].addEventListener("click",e => {
-    let idx = tabs.findIndex(elem => elem.classList.contains("tab_active"));
     tabs[0].classList.remove("tab_active");
     tabs[1].classList.remove("tab_active");
     tabs[2].classList.remove("tab_active");
     e.target.classList.add("tab_active");
+    let idx = tabs.findIndex(elem => elem.classList.contains("tab_active"));
      tabContents[0].classList.remove("tab__content_active");
      tabContents[1].classList.remove("tab__content_active");
      tabContents[2].classList.remove("tab__content_active");
